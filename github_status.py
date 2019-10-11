a42d6430128a8a2c0ac79cee0d4670a8bb2415bc
Dominik Mueller <me@muellerdominik.ch>
2019-10-11 16:34:02 +0200

    Changed to single quotes.

--- github_status.py
+++ github_status.py
@@ -7,24 +7,26 @@ from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
 # OAuth Token (user scope required)
-api_token = "<access token>"
+api_token = '<access token>'
 
 # Timezone
 time_zone = 'Europe/Zurich'
 _tz = timezone(time_zone)
 
 # GitHub GraphQL API v4
-endpoint = "https://api.github.com/graphql"
+endpoint = 'https://api.github.com/graphql'
 
 # Transport
 _transport = RequestsHTTPTransport(
     url=endpoint,
     use_json=True,
-    headers={"Authorization": f"bearer {api_token}"})
+    headers={'Authorization': f'bearer {api_token}'}
+)
 
 # Client
 client = Client(
-    transport=_transport)
+    transport=_transport
+)
 
 # Current time
 now = datetime.now(tz=_tz)
@@ -32,6 +34,7 @@ now = datetime.now(tz=_tz)
 # Emojis require 1-12 and not 0-11
 tf = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
+# Suffix for 'clock' emoji
 time = f'{tf[now.hour % 12]}'
 if now.minute >= 30:
     time = f'{time}30'
@@ -48,3 +51,9 @@ mutation ChangeStatusEmoji {{
 ''')
 
 client.execute(query)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
