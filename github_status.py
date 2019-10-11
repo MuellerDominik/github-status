9c747904a3a51b46428dc24198a00c134fd029ac
Dominik Mueller <me@muellerdominik.ch>
2019-10-11 21:25:09 +0200

    Added periodic execution

--- github_status.py
+++ github_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
+from time import sleep
+
 from pytz import timezone
 from datetime import datetime
 
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
-# OAuth Token (user scope required)
+# OAuth token (user scope required)
 api_token = '<access token>'
 
-# Timezone
+# Time zone
 time_zone = 'Europe/Zurich'
 _tz = timezone(time_zone)
 
@@ -28,32 +30,54 @@ client = Client(
     transport=_transport
 )
 
-# Current time
-now = datetime.now(tz=_tz)
+def curr_time():
+    # Current time
+    return datetime.now(tz=_tz)
 
-# Emojis require 1-12 and not 0-11
-tf = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+def time_suffix(now):
+    # Emojis require 1-12 and not 0-11
+    tf = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-# Suffix for 'clock' emoji
-time = f'{tf[now.hour % 12]}'
-if now.minute >= 30:
-    time = f'{time}30'
+    # Suffix for 'clock' emoji
+    time = f'{tf[now.hour % 12]}'
+    if now.minute >= 30:
+        time = f'{time}30'
 
-# GraphQL query
-query = gql(f'''
-mutation ChangeStatusEmoji {{
-  changeUserStatus(input: {{emoji: ":clock{time}:"}}) {{
-    status {{
-      emoji
+    return time
+
+def update_status_emoji(time):
+    # GraphQL query
+    query = gql(f'''
+    mutation ChangeStatusEmoji {{
+      changeUserStatus(input: {{emoji: ":clock{time}:"}}) {{
+        status {{
+          emoji
+        }}
+      }}
     }}
-  }}
-}}
-''')
+    ''')
 
-client.execute(query)
+    # Execute query and return result
+    return client.execute(query)
 
 def main():
-    pass
+    while True:
+        # Update the status emoji
+        now = curr_time()
+        ts = time_suffix(now)
+        transaction = update_status_emoji(ts)
+
+        # Transaction log
+        with open('transaction.log', 'a') as f:
+            f.write(str(now) + ': ' + str(transaction) + '\n')
+
+        # Sleep until the next hour or half-hour
+        now = curr_time()
+        sleep_time = 30 - now.minute
+        if now.minute >= 30:
+            sleep_time += 30
+
+        sleep(sleep_time * 60 - now.second)
 
 if __name__ == '__main__':
     main()
